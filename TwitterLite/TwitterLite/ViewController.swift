e8b6ea86d60a79914728086d66432889d7a11ceb
rahulsingh2k10 <rahulsingh2k10@gmail.com>
2022-06-23 22:26:41 +0530

    Initial Project setup along with CocoPod added.

---
+++ TwitterLite/TwitterLite/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterLite
+//
+//  Created by Rahul Singh on 23/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
