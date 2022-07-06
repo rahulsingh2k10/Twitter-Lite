efa12c8632c4c97fbfc69d604d3c5de151ccf9bf
rahulsingh2k10 <rahulsingh2k10@gmail.com>
2022-07-07 00:00:39 +0530

    1. Hiding delete button in case of view mode. 2. Added activity indicator in case image is getting downloaded.

--- TwitterLite/TwitterLite/Common/Views/PhotoFrameCollectionViewCell.swift
+++ TwitterLite/TwitterLite/Common/Views/PhotoFrameCollectionViewCell.swift
@@ -6,21 +6,44 @@
 //
 
 import UIKit
+import SDWebImage
 
 
+@IBDesignable
 class PhotoFrameCollectionViewCell: UICollectionViewCell, NibReusable {
     public var deleteButtonCallback: ((UICollectionViewCell) -> ())?
+
+    @IBInspectable var borderWidth: CGFloat = 1.0
+    @IBInspectable var cornerRadius: CGFloat = 8.0
+    @IBInspectable var borderColor: UIColor = .placeholderText
 
     @IBOutlet weak private var deleteButton: UIButton!
     @IBOutlet weak private var photoImageView: UIImageView!
 
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+
+        layer.cornerRadius = cornerRadius
+        layer.borderWidth = borderWidth
+        layer.borderColor = borderColor.cgColor
+        layer.masksToBounds = true
+    }
+
     //MARK: - Public Methods -
-    public func loadImage(imageData: Any) {
+    public func loadImage(imageData: Any, hideDeleteButton: Bool = false) {
+        deleteButton.isHidden = hideDeleteButton
+
         if imageData is ImageWrapper {
             photoImageView.image = (imageData as? ImageWrapper)?.image
         } else {
-            photoImageView.sd_setImage(with: imageData as? URL,
-                                       placeholderImage: UIImage(systemName: "Placeholder"))
+            if let urlString = imageData as? String,
+               let url = URL(string: urlString) {
+                photoImageView.sd_imageIndicator = SDWebImageActivityIndicator.medium
+                photoImageView.sd_setImage(with: url,
+                                           placeholderImage: UIImage(systemName: "Placeholder"))
+            } else {
+                photoImageView.image = UIImage(systemName: "Placeholder")
+            }
         }
     }
 
